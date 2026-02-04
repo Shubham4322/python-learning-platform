68726723d4f244ff89c573a4ad059ac01bf6ebbd
Shubham Bhingude <shubhambhingude4846@gmail.com>
2026-02-04 11:12:33 +0530

    Ready for Render deployment

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,14 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Run migrations (This creates/updates your db.sqlite3 file)
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin@123')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+EOF
